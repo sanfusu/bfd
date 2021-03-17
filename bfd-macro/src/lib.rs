08faf2706d159f342464a2f76af291d4c0085a88
sanfusu <sanfusu@outlook.com>
2021-03-17 23:13:55 +0800

    new

---
+++ bfd-macro/src/lib.rs
@@ -0,0 +1,130 @@
+use quote::{format_ident, quote};
+use syn::{parse_macro_input, DeriveInput};
+
+#[proc_macro_derive(Fields)]
+pub fn fields_derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+    let layout = generate_layout(input);
+    layout.into()
+}
+
+fn generate_layout(ast: syn::DeriveInput) -> proc_macro2::TokenStream {
+    let struct_ident = ast.ident;
+    if struct_ident.to_string().contains("Meta") == false {
+        core::panic!("The struct derived Bdf should be named postfix Meta")
+    }
+    let fields_trait_name = format_ident!(
+        "{}Fields",
+        struct_ident.to_string().strip_suffix("Meta").unwrap()
+    );
+    let struct_plain_name =
+        format_ident!("{}", struct_ident.to_string().strip_suffix("Meta").unwrap());
+    let mut fields_id = Vec::<syn::Ident>::new();
+    let mut fields_ty = Vec::<syn::Type>::new();
+    if let syn::Data::Struct(data) = ast.data {
+        data.fields.iter().for_each(|x| {
+            fields_id.push(x.ident.to_owned().unwrap());
+            fields_ty.push(x.ty.to_owned());
+        });
+    }
+    let mut fields_range = Vec::<proc_macro2::TokenStream>::new();
+    let first_ty = fields_ty[0].to_owned();
+    fields_range.push(quote!(0..core::mem::size_of::<#first_ty>()));
+    fields_id[0..]
+        .iter()
+        .zip(fields_ty[1..].iter())
+        .for_each(|(id,ty)| {
+            fields_range.push(quote!(<#id>::layout_range().end..<#id>::layout_range().end + core::mem::size_of::<#ty>()))
+        });
+
+    let struct_size = fields_ty.iter().fold(quote!(0), |mut acc, ty| {
+        acc.extend(quote!(+ core::mem::size_of::<#ty>()));
+        acc
+    });
+    quote! {
+        mod bfd {
+            pub struct #struct_plain_name<'a, End: crate::Endianess> {
+                raw: &'a [u8; #struct_size],
+                phantom: core::marker::PhantomData<End>
+            }
+            impl<'a> #struct_plain_name<'a, crate::Le> {
+                pub fn get<T: fields::#fields_trait_name + crate::ByteOrder>(&self)-> T {
+                    T::from_le_bytes(&self.raw[T::layout_range()])
+                }
+                // pub fn to_meta(&self)-> super::#struct_ident {
+                //     super::#struct_ident {
+                //         #(
+                //             #fields_id: self.get::<#fields_id>(),
+                //         )*
+                //     }
+                // }
+            }
+            pub mod fields {
+                use core::convert::TryInto;
+                pub trait #fields_trait_name {
+                    fn layout_range()->core::ops::Range<usize>;
+                }
+                #(
+                    #[allow(non_camel_case_types)]
+                    pub struct #fields_id {
+                        value: #fields_ty
+                    }
+                    impl #fields_trait_name for #fields_id{
+                        #[inline]
+                        fn layout_range()->core::ops::Range<usize> {
+                            #fields_range
+                        }
+                    }
+                    impl #fields_id {
+                        fn new(value: #fields_ty)-> #fields_id {
+                            #fields_id {
+                                value
+                            }
+                        }
+                    }
+                    impl crate::ByteOrder for #fields_id {
+                        type Bytes = [u8; core::mem::size_of::<#fields_ty>()];
+                        fn to_ne_bytes(&self) -> [u8; core::mem::size_of::<#fields_ty>()] {
+                            #fields_ty::to_ne_bytes(self.value)
+                        }
+                        fn to_le_bytes(&self) -> [u8; core::mem::size_of::<#fields_ty>()] {
+                            #fields_ty::to_le_bytes(self.value)
+                        }
+                        fn to_be_bytes(&self) -> [u8; core::mem::size_of::<#fields_ty>()] {
+                            #fields_ty::to_be_bytes(self.value)
+                        }
+                        fn from_le_bytes(x: &[u8]) -> Self {
+                            Self {value:#fields_ty::from_le_bytes(x.try_into().unwrap())}
+                        }
+                        fn from_be_bytes(x: &[u8]) -> Self {
+                            Self {value:#fields_ty::from_be_bytes(x.try_into().unwrap())}
+                        }
+                        fn from_ne_bytes(x: &[u8]) -> Self {
+                            Self {value:#fields_ty::from_ne_bytes(x.try_into().unwrap())}
+                        }
+                        fn from_be(x: Self) -> Self {
+                            Self {
+                                value: #fields_ty::from_be(x.value)
+                            }
+                        }
+                        fn from_le(x: Self) -> Self {
+                            Self {
+                                value: #fields_ty::from_le(x.value)
+                            }
+                        }
+                        fn to_be(self) -> Self {
+                            Self {
+                                value: #fields_ty::to_be(self.value)
+                            }
+                        }
+                        fn to_le(self) -> Self {
+                            Self {
+                                value: #fields_ty::to_le(self.value)
+                            }
+                        }
+                    }
+                )*
+            }
+        }
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,46 @@
+#![feature(const_fn)]
+#[macro_use]
+extern crate bfd_macro;
+
+pub trait ByteOrder {
+    type Bytes;
+    fn to_ne_bytes(&self) -> Self::Bytes;
+    fn to_le_bytes(&self) -> Self::Bytes;
+    fn to_be_bytes(&self) -> Self::Bytes;
+    fn from_ne_bytes(x: &[u8]) -> Self;
+    fn from_le_bytes(x: &[u8]) -> Self;
+    fn from_be_bytes(x: &[u8]) -> Self;
+    fn from_be(x: Self) -> Self;
+    fn from_le(x: Self) -> Self;
+    fn to_be(self) -> Self;
+    fn to_le(self) -> Self;
+}
+pub trait Endianess {}
+pub struct Le;
+impl Endianess for Le {}
+pub struct Be;
+impl Endianess for Be {}
+// impl ByteOrder for u32 {
+//     type Bytes = [u8;4];
+//     fn to_ne_bytes(&self) -> [u8;4] {
+//         todo!()
+//     }
+// }
+
+#[derive(Fields)]
+pub struct TestMeta {
+    pub field1: u32,
+    pub field2: u8,
+}
+
+pub use bfd::fields::*;
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn layout_test() {
+        println!("{:?}", field1::layout_range());
+        println!("{:?}", field2::layout_range());
+    }
+}
