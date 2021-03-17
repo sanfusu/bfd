ee6d79f5b2bebd108b06fc6cf3c8d6db1da52bc8
sanfusu <sanfusu@outlook.com>
2021-03-18 00:13:26 +0800

    more work to make it work

--- bfd-macro/src/lib.rs
+++ bfd-macro/src/lib.rs
@@ -42,23 +42,43 @@ fn generate_layout(ast: syn::DeriveInput) -> proc_macro2::TokenStream {
         acc
     });
     quote! {
-        mod bfd {
+        mod bfd_field {
             use core::convert::TryInto;
-            pub struct #struct_plain_name<'a, End: crate::Endianess> {
+            pub struct #struct_plain_name<'a, End: crate::bfd::Endianess> {
                 raw: &'a [u8; #struct_size],
                 phantom: core::marker::PhantomData<End>
             }
-            impl<'a> #struct_plain_name<'a, crate::Le> {
-                pub fn get<T: fields::#fields_trait_name + crate::ByteOrder<'a>>(&self)-> T {
+            impl<'a, End: crate::bfd::Endianess> #struct_plain_name<'a, End> {
+                pub fn new(raw:  &'a [u8; #struct_size])->Self {
+                    Self {
+                        raw,
+                        phantom: core::marker::PhantomData
+                    }
+                }
+            }
+            impl<'a> #struct_plain_name<'a, crate::bfd::Le> {
+                pub fn get<T: fields::#fields_trait_name + crate::bfd::ByteOrder<'a>>(&self)-> T {
                     T::from_le_bytes((&self.raw[T::layout_range()]).try_into().unwrap())
                 }
-                // pub fn to_meta(&self)-> super::#struct_ident {
-                //     super::#struct_ident {
-                //         #(
-                //             #fields_id: self.get::<#fields_id>(),
-                //         )*
-                //     }
-                // }
+                pub fn to_meta(&self)-> super::#struct_ident {
+                    super::#struct_ident {
+                        #(
+                            #fields_id: self.get::<fields::#fields_id>().raw(),
+                        )*
+                    }
+                }
+            }
+            impl<'a> #struct_plain_name<'a, crate::bfd::Be> {
+                pub fn get<T: fields::#fields_trait_name + crate::bfd::ByteOrder<'a>>(&self)-> T {
+                    T::from_be_bytes((&self.raw[T::layout_range()]).try_into().unwrap())
+                }
+                pub fn to_meta(&self)-> super::#struct_ident {
+                    super::#struct_ident {
+                        #(
+                            #fields_id: self.get::<fields::#fields_id>().raw(),
+                        )*
+                    }
+                }
             }
             pub mod fields {
                 use core::convert::TryInto;
@@ -67,6 +87,7 @@ fn generate_layout(ast: syn::DeriveInput) -> proc_macro2::TokenStream {
                 }
                 #(
                     #[allow(non_camel_case_types)]
+                    #[derive(Debug)]
                     pub struct #fields_id {
                         value: #fields_ty
                     }
@@ -82,8 +103,11 @@ fn generate_layout(ast: syn::DeriveInput) -> proc_macro2::TokenStream {
                                 value
                             }
                         }
+                        pub fn raw(&self)->#fields_ty {
+                            self.value
+                        }
                     }
-                    impl<'a> crate::ByteOrder<'a> for #fields_id {
+                    impl<'a> crate::bfd::ByteOrder<'a> for #fields_id {
                         type Bytes = [u8; core::mem::size_of::<#fields_ty>()];
                         fn to_ne_bytes(&self) -> [u8; core::mem::size_of::<#fields_ty>()] {
                             #fields_ty::to_ne_bytes(self.value)
